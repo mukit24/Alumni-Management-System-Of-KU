5e0e1e46c4dc7b54e2a8e5ac24974711a43620aa
mukit24 <mukithasanpranto4@gmail.com>
2022-11-29 23:42:37 +0600

    ready_for_deployment

--- AMSKU/settings.py
+++ AMSKU/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-akqv-!1ei!m^5k0_2!r#0=fgnvyvg2se3dt1r--x*y+1kj1sm!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -159,6 +160,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_ROOT =  BASE_DIR / "media"
 MEDIA_URL = '/media/'
